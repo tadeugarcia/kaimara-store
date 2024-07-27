782edaf07c2b56bee19e0ee245bbff81173d0873
Tadeu Garcia <tadeugarcia.92@gmail.com>
2024-07-27 20:09:15 -0300

    traduções

--- src/modules/products/components/product-price/index.tsx
+++ src/modules/products/components/product-price/index.tsx
@@ -35,7 +35,7 @@ export default function ProductPrice({
           "text-ui-fg-interactive": selectedPrice.price_type === "sale",
         })}
       >
-        {!variant && "From "}
+        {!variant && "A partir de "}
         <span
           data-testid="product-price"
           data-value={selectedPrice.calculated_price_number}
