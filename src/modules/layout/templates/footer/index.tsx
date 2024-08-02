9fb0ba72c9f84f58c313bc20e0d727b3531114ef
Tadeu Garcia <tadeugarcia.92@gmail.com>
2024-08-02 19:31:05 -0300

    estilo

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -3,7 +3,7 @@ import { Text } from "@medusajs/ui"
 export default async function Footer() {
   return (
     <footer className="w-full py-8 bg-slate-100">
-      <Text className="txt-compact-small">
+      <Text className="txt-compact-small text-center text-ui-fg-muted">
         © Kaimara {new Date().getFullYear()}
       </Text>
     </footer>
