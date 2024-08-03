9be2b6e66b54203fc3b099adac1a9e9884d6ff77
Tadeu Garcia <tadeugarcia.92@gmail.com>
2024-08-02 22:43:32 -0300

    add product.subtitle

--- src/modules/products/templates/product-info/index.tsx
+++ src/modules/products/templates/product-info/index.tsx
@@ -21,6 +21,10 @@ const ProductInfo = ({ product }: ProductInfoProps) => {
         <Heading level="h2" className="text-3xl leading-10 text-ui-fg-base" data-testid="product-title">
           {product.title}
         </Heading>
+        
+        <Heading level="h3" className="text-medium text-ui-fg-subtle" data-testid="product-subtitle">
+          {product.subtitle}
+        </Heading>
 
         <Text className="text-medium text-ui-fg-subtle" data-testid="product-description">
           {product.description}
